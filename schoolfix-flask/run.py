db5cfd79226ce4a328c7664d910bcc8dc2ef6f59
matheusfelipext <t1196174@gmail.com>
2026-09-16 16:16:46 -0300

    Modify app.run to use dynamic port configuration
    Updated run.py to set the port dynamically based on environment variable.

--- schoolfix-flask/run.py
+++ schoolfix-flask/run.py
@@ -1,9 +1,12 @@
+import os
 from dotenv import load_dotenv
-load_dotenv()
-
 from app import create_app
+
+load_dotenv()
 
 app = create_app()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    # Captura a porta atribuída pelo Render (ou usa 5000 localmente)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
